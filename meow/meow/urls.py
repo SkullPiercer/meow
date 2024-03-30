1384ca7c0e823873e54ff3df027fda2b1d994cb0
Александр Денисов <akatsuki_sanya@vk.com>
2024-03-30 23:05:39 +0500

    Add a sign in and registration buttons

--- meow/meow/urls.py
+++ meow/meow/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('django.contrib.auth.urls')),
     path('', include('cats.urls', namespace='cat')),
 ]
