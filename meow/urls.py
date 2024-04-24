b9e56b10776a12b5e1d66048e5bdd9fc24b1cf1e
Александр Денисов <akatsuki_sanya@vk.com>
2024-04-24 15:19:36 +0500

    Add a registration page and form

--- meow/urls.py
+++ meow/urls.py
@@ -1,11 +1,22 @@
+from django.contrib import admin
+from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
+from django.views.generic.edit import CreateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('django.contrib.auth.urls')),
+    path(
+        'auth/registration',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('pages:home')
+        ),
+        name='registration'
+    ),
     path('', include('pages.urls', namespace='pages')),
     path('cats/', include('cats.urls', namespace='cat')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
